f4f02cd52fd05120b3ed2cd3e30bd582b7f533b0
lijinhong11 <tygfhk@outlook.com>
2024-03-24 21:37:06 +0800

    小改

--- src/main/java/me/ddggdd135/slimeae/api/interfaces/MEBus.java
+++ src/main/java/me/ddggdd135/slimeae/api/interfaces/MEBus.java
@@ -49,7 +49,7 @@ public abstract class MEBus extends SlimefunItem implements IMEObject<MEBus>, In
     private final int DOWN_SLOT = 33;
 
     private final int[] backgroundSlots = new int[] {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16, 17, 18, 19, 21, 23, 24, 25, 26, 27, 28, 29, 21, 31, 32, 34,
+        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16, 17, 18, 19, 21, 23, 24, 25, 26, 27, 28, 29, 31, 32, 34,
         35, 36, 37, 38, 39, 40, 41, 42, 43, 44
     };
 
@@ -80,7 +80,7 @@ public abstract class MEBus extends SlimefunItem implements IMEObject<MEBus>, In
                 new BlockTicker() {
                     @Override
                     public boolean isSynchronized() {
-                        return true;
+                        return false;
                     }
 
                     @Override
