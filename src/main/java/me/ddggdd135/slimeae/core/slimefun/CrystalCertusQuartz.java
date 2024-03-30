c25e945a6ce58b1a47cca6a82d8b32406a9eb8a1
shixinzia <shixinzia@gmail.com>
2024-03-30 10:14:01 +0800

    添加部分ItemStack

---
+++ src/main/java/me/ddggdd135/slimeae/core/slimefun/CrystalCertusQuartz.java
@@ -0,0 +1,56 @@
+package me.ddggdd135.slimeae.core.slimefun;
+
+import io.github.thebusybiscuit.slimefun4.api.geo.GEOResource;
+import org.bukkit.NamespacedKey;
+import org.bukkit.World;
+import org.bukkit.block.Biome;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+
+public class CrystalCertusQuartz implements GEOResource {
+
+    private final NamespacedKey key;
+    private final ItemStack item;
+
+    public CrystalCertusQuartz(Plugin plugin, ItemStack item) {
+        this.key = new NamespacedKey(plugin, "certus_quartz");
+        this.item = item;
+    }
+
+    @Override
+    public NamespacedKey getKey() {
+        return key;
+    }
+
+    @Override
+    public ItemStack getItem() {
+        return item.clone();
+    }
+
+    @Override
+    public String getName() {
+        return "赛特斯石英水晶";
+    }
+
+    @Override
+    public boolean isObtainableFromGEOMiner() {
+        return true;
+    }
+
+    @Override
+    public int getDefaultSupply(World.Environment environment, Biome biome) {
+
+        if (environment == World.Environment.NORMAL) {
+            return 5;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    @Override
+    public int getMaxDeviation() {
+        return 3;
+    }
+
+}
