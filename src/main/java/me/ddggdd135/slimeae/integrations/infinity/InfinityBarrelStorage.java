b508234a6cff2399fe445f666be3f792f760e245
Ddggdd135 <1306334428@qq.com>
2024-04-14 19:50:54 +0800

    添加缺失文件

---
+++ src/main/java/me/ddggdd135/slimeae/integrations/infinity/InfinityBarrelStorage.java
@@ -0,0 +1,96 @@
+package me.ddggdd135.slimeae.integrations.infinity;
+
+import com.xzavier0722.mc.plugin.slimefun4.storage.controller.SlimefunBlockData;
+import com.xzavier0722.mc.plugin.slimefun4.storage.util.StorageCacheUtils;
+import io.github.mooy1.infinityexpansion.items.storage.StorageCache;
+import io.github.mooy1.infinityexpansion.items.storage.StorageUnit;
+import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Nonnull;
+import me.ddggdd135.slimeae.SlimeAEPlugin;
+import me.ddggdd135.slimeae.api.ItemRequest;
+import me.ddggdd135.slimeae.api.ItemStorage;
+import me.ddggdd135.slimeae.api.interfaces.IStorage;
+import me.ddggdd135.slimeae.utils.ItemUtils;
+import me.ddggdd135.slimeae.utils.ReflectionUtils;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class InfinityBarrelStorage implements IStorage {
+    private StorageCache cache;
+    private boolean isReadOnly;
+
+    public InfinityBarrelStorage(@Nonnull Block block) {
+        this(block, false);
+    }
+
+    public InfinityBarrelStorage(@Nonnull Block block, boolean isReadOnly) {
+        if (!SlimeAEPlugin.getInfinityIntegration().isLoaded()) throw new RuntimeException("Infinity is not loaded");
+        SlimefunBlockData blockData = StorageCacheUtils.getBlock(block.getLocation());
+        if (blockData != null && SlimefunItem.getById(blockData.getSfId()) instanceof StorageUnit storageUnit) {
+            cache = storageUnit.getCache(block.getLocation());
+        }
+        this.isReadOnly = isReadOnly;
+    }
+
+    @Override
+    public void pushItem(@Nonnull ItemStack[] itemStacks) {
+        if (!isReadOnly && cache != null) cache.depositAll(itemStacks);
+    }
+
+    @Override
+    public boolean contains(@Nonnull ItemRequest[] requests) {
+        if (cache == null) return false;
+        boolean toReturn = true;
+        for (ItemRequest request : requests) {
+            if (Boolean.TRUE.equals(ReflectionUtils.invokePrivateMethod(
+                    cache, "matches", new Class<?>[] {ItemStack.class}, request.getTemplate()))) {
+                int amount = ReflectionUtils.getField(cache, "amount");
+                toReturn = toReturn && amount >= request.getAmount();
+            }
+        }
+        return toReturn;
+    }
+
+    @Nonnull
+    @Override
+    public ItemStack[] tryTakeItem(@Nonnull ItemRequest[] requests) {
+        if (cache == null) return new ItemStack[0];
+        ItemStorage toReturn = new ItemStorage();
+        for (ItemRequest request : requests) {
+            if (Boolean.TRUE.equals(ReflectionUtils.invokePrivateMethod(
+                    cache, "matches", new Class<?>[] {ItemStack.class}, request.getTemplate()))) {
+                int amount = ReflectionUtils.getField(cache, "amount");
+                int toTake = Math.min(amount, request.getAmount());
+                if (toTake != 0) {
+                    ReflectionUtils.setField(cache, "amount", amount - toTake);
+                    toReturn.addItem(ItemUtils.createItems(request.getTemplate(), toTake));
+                }
+            }
+        }
+        return toReturn.toItemStacks();
+    }
+
+    @Override
+    public @Nonnull Map<ItemStack, Integer> getStorage() {
+        Map<ItemStack, Integer> storage = new HashMap<>();
+        Material material = ReflectionUtils.getField(cache, "material");
+        ItemMeta meta = ReflectionUtils.getField(cache, "meta");
+
+        if (cache == null || material == null || ReflectionUtils.<Integer>getField(cache, "amount") == 0)
+            return storage;
+        ItemStack itemStack = new ItemStack(material);
+        if (meta != null) itemStack.setItemMeta(meta);
+
+        storage.put(itemStack, ReflectionUtils.getField(cache, "amount"));
+        return storage;
+    }
+
+    @Override
+    public int getEmptySlots() {
+        return 0;
+    }
+}
---
+++ src/main/java/me/ddggdd135/slimeae/utils/ReflectionUtils.java
@@ -0,0 +1,102 @@
+package me.ddggdd135.slimeae.utils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class ReflectionUtils {
+    /**
+     * 使用反射调用对象的私有方法，并返回泛型类型的值。
+     *
+     * @param object        要调用方法的对象实例
+     * @param methodName 私有方法的名称
+     * @param args       调用方法时传递的参数，如果方法不需要参数，则传递空数组或null
+     * @param <T>        返回值的泛型类型
+     * @return 方法的返回值，如果方法返回void或调用失败，则返回null
+     * @throws RuntimeException 如果反射调用失败
+     */
+    @Nullable public static <T> T invokePrivateMethod(
+            @Nonnull Object object,
+            @Nonnull String methodName,
+            @Nonnull Class<?>[] parameterTypes,
+            @Nonnull Object... args) {
+        try {
+            Class<?> clazz = object.getClass();
+            // 获取Method对象
+            Method method = clazz.getDeclaredMethod(methodName, parameterTypes);
+
+            // 设置访问权限为true，允许访问私有方法
+            method.setAccessible(true);
+
+            // 调用方法
+            return (T) method.invoke(object, args);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException("No such method: " + methodName, e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Illegal access to method: " + methodName, e);
+        } catch (InvocationTargetException e) {
+            Throwable targetException = e.getTargetException();
+            if (targetException instanceof RuntimeException) {
+                throw (RuntimeException) targetException;
+            } else {
+                throw new RuntimeException("Invocation target exception in method: " + methodName, targetException);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Exception in method invocation: " + methodName, e);
+        }
+    }
+
+    @Nullable public static <T> T getField(@Nonnull Object object, @Nonnull String fieldName) {
+        try {
+            Field[] fields = getAllFields(object);
+            Field field = null;
+            for (Field f : fields) {
+                if (f.getName().equals(fieldName)) {
+                    field = f;
+                    break;
+                }
+            }
+            if (field == null) throw new NoSuchFieldException(fieldName);
+            field.setAccessible(true);
+            return (T) field.get(object);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static <T> void setField(@Nonnull Object object, @Nonnull String fieldName, @Nullable T value) {
+        try {
+            Field[] fields = getAllFields(object);
+            Field field = null;
+            for (Field f : fields) {
+                if (f.getName().equals(fieldName)) {
+                    field = f;
+                    break;
+                }
+            }
+            if (field == null) throw new NoSuchFieldException(fieldName);
+            field.setAccessible(true);
+            field.set(object, value);
+        } catch (NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Nonnull
+    public static Field[] getAllFields(@Nonnull Object object) {
+        Class<?> clazz = object.getClass();
+        List<Field> fieldList = new ArrayList<>();
+        while (clazz != null) {
+            fieldList.addAll(new ArrayList<>(Arrays.asList(clazz.getDeclaredFields())));
+            clazz = clazz.getSuperclass();
+        }
+        Field[] fields = new Field[fieldList.size()];
+        fieldList.toArray(fields);
+        return fields;
+    }
+}
