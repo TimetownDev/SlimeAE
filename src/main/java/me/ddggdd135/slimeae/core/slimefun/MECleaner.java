bc85f3a35e728c6da1d52094f95fd05d69312b4b
Ddggdd135 <1306334428@qq.com>
2025-02-11 22:43:31 +0800

    修复ME清除器 物品识别错误

--- src/main/java/me/ddggdd135/slimeae/core/slimefun/MECleaner.java
+++ src/main/java/me/ddggdd135/slimeae/core/slimefun/MECleaner.java
@@ -172,8 +172,7 @@ public class MECleaner extends TickingBlock implements IMEObject, InventoryBlock
 
     public int[] getSettingSlots() {
         return new int[] {
-            10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 33, 35, 37, 38, 39, 40, 41, 42,
-            43
+            10, 11, 12, 13, 14, 15, 16, 19, 20, 21, 22, 23, 24, 25, 28, 29, 30, 31, 32, 33, 37, 38, 39, 40, 41, 42, 43
         };
     }
 }
