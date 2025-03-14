539c076fedc35bace51377ed6fe94e124e7a5018
Ddggdd135 <1306334428@qq.com>
2025-03-15 06:49:21 +0800

    Revert "Revert "更换物品HashMap实现""
    This reverts commit 08f3fe07c4875d484b20f9c647102a46ef8d4a07.

--- src/main/java/me/ddggdd135/slimeae/api/items/MEStorageCellCache.java
+++ src/main/java/me/ddggdd135/slimeae/api/items/MEStorageCellCache.java
@@ -5,6 +5,7 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import me.ddggdd135.guguslimefunlib.api.ItemHashMap;
 import me.ddggdd135.guguslimefunlib.libraries.nbtapi.NBT;
 import me.ddggdd135.guguslimefunlib.libraries.nbtapi.NBTType;
 import me.ddggdd135.slimeae.SlimeAEPlugin;
@@ -27,7 +28,7 @@ public class MEStorageCellCache implements IStorage {
         size = MEItemStorageCell.getSize(itemStack);
         if (SlimefunItem.getByItem(itemStack) instanceof MECreativeItemStorageCell) storages = new CreativeItemMap();
         else {
-            storages = new ConcurrentHashMap<>();
+            storages = new ItemHashMap<>();
         }
         UUID tmp = NBT.get(itemStack, x -> {
             return x.getUUID(MEItemStorageCell.UUID_KEY);
@@ -152,7 +153,7 @@ public class MEStorageCellCache implements IStorage {
     @Override
     public @Nonnull Map<ItemStack, Long> getStorage() {
         if (storages instanceof CreativeItemMap) return storages;
-        return new HashMap<>(storages);
+        return new ItemHashMap<>(storages);
     }
 
     public Map<ItemStack, Long> getSourceStorage() {
