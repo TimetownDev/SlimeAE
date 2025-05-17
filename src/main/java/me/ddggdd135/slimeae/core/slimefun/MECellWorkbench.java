6436a869404b16e8f6f99dbb1d4191facb573a68
Ddggdd135 <1306334428@qq.com>
2025-05-17 16:36:41 +0800

    为元件工作台添加7格

--- src/main/java/me/ddggdd135/slimeae/core/slimefun/MECellWorkbench.java
+++ src/main/java/me/ddggdd135/slimeae/core/slimefun/MECellWorkbench.java
@@ -53,13 +53,13 @@ public class MECellWorkbench extends SlimefunItem implements InventoryBlock {
     }
 
     public int[] getBorderSlots() {
-        return new int[] {0, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 25, 34, 35, 43, 44, 52, 53};
+        return new int[] {0, 2, 3, 4, 5, 6, 7, 8, 25, 34, 35, 43, 44, 52, 53};
     }
 
     public int[] getSettingSlots() {
         return new int[] {
-            18, 19, 20, 21, 22, 23, 24, 27, 28, 29, 30, 31, 32, 33, 36, 37, 38, 39, 40, 41, 42, 45, 46, 47, 48, 49, 50,
-            51
+            9, 10, 11, 12, 13, 14, 15, 16, 18, 19, 20, 21, 22, 23, 24, 27, 28, 29, 30, 31, 32, 33, 36, 37, 38, 39, 40,
+            41, 42, 45, 46, 47, 48, 49, 50, 51
         };
     }
 
