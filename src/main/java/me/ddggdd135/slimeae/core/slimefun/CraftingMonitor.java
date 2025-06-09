3dd02f25d86da9788e4b342c78dd05fcf78e7536
Ddggdd135 <1306334428@qq.com>
2025-06-09 13:58:09 +0800

    添加遗失的权限检查

--- src/main/java/me/ddggdd135/slimeae/core/slimefun/CraftingMonitor.java
+++ src/main/java/me/ddggdd135/slimeae/core/slimefun/CraftingMonitor.java
@@ -5,6 +5,8 @@ import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem;
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack;
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockUseHandler;
+import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.protection.Interaction;
 import me.ddggdd135.slimeae.SlimeAEPlugin;
 import me.ddggdd135.slimeae.api.interfaces.IMEObject;
 import me.ddggdd135.slimeae.core.NetworkInfo;
@@ -26,6 +28,13 @@ public class CraftingMonitor extends SlimefunItem implements IMEObject {
     private BlockUseHandler onRightClick() {
         return e -> {
             Block block = e.getClickedBlock().get();
+            if (!(e.getPlayer().hasPermission("slimefun.inventory.bypass")
+                    || Slimefun.getProtectionManager()
+                            .hasPermission(e.getPlayer(), block.getLocation(), Interaction.INTERACT_BLOCK))) {
+                Slimefun.getLocalization().sendMessage(e.getPlayer(), "inventory.no-access", true);
+                return;
+            }
+
             NetworkInfo info = SlimeAEPlugin.getNetworkData().getNetworkInfo(block.getLocation());
             if (info == null) return;
             info.openAutoCraftingSessionsMenu(e.getPlayer());
